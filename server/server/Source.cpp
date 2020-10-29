a2486cf6cd29dd44f822f6369cadc8e4e53bb54a
kkillof <ilyakukingd@gmail.com>
2020-10-29 16:27:22 +0300

    Добавлен проект server

---
+++ server/server/Source.cpp
@@ -0,0 +1,36 @@
+#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable:4996)
+
+int main(int argc, char* argv[]) {
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		std::cout << "Error" << std::endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&addr, sizeofaddr);
+	listen(sListen, SOMAXCONN);
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+	if (newConnection == 0) {
+		std::cout << "Error #2\n";
+	}
+	else {
+		std::cout << "Client Connected!\n";
+	}
+	system("pause");
+	return 0;
+}
